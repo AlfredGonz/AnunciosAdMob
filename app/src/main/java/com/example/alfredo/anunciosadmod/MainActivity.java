9385bd945be19a6dbbb9fd25a43ea027d14435e9
unknown <manu.amg95@gmail.com>
2018-11-16 09:53:17 -0600

    anuncios

---
+++ app/src/main/java/com/example/alfredo/anunciosadmod/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.alfredo.anunciosadmod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private AdView mAdView;
+    // ...
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
